de3d571032a4da123f258cb3f05721f0dc83ccc7
ibdkn <ibudiakina@gmail.com>
2024-11-15 22:41:03 +0300

    remove data from db

--- dist/db/db.js
+++ dist/db/db.js
@@ -2,46 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setPostDB = exports.setBlogDB = exports.db = void 0;
 exports.db = {
-    blogs: [
-        {
-            id: '1',
-            name: 'Blog 1',
-            description: 'First blog',
-            websiteUrl: 'https://samurai-blog-1.com',
-        },
-        {
-            id: '2',
-            name: 'Blog 2',
-            description: 'Second blog',
-            websiteUrl: 'https://samurai-blog-2.com'
-        }
-    ],
-    posts: [
-        {
-            id: '1',
-            title: 'Post 1',
-            shortDescription: 'description for post 1',
-            content: 'content for post 1',
-            blogId: '1',
-            blogName: 'Blog 1',
-        },
-        {
-            id: '2',
-            title: 'Post 2',
-            shortDescription: 'description for post 2',
-            content: 'content for post 2',
-            blogId: '1',
-            blogName: 'Blog 1',
-        },
-        {
-            id: '3',
-            title: 'Post 3',
-            shortDescription: 'description for post 3',
-            content: 'content for post 3',
-            blogId: '2',
-            blogName: 'Blog 2',
-        },
-    ]
+    blogs: [],
+    posts: []
 };
 // функции для быстрой очистки/заполнения базы данных для тестов
 const setBlogDB = (dataset) => {
