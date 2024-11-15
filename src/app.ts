3d986e7bf4d0b420622523adbe82715270d69e88
ibdkn <ibudiakina@gmail.com>
2024-11-15 22:40:21 +0300

    remove data from db

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Router} from 'express';
+import express, {Request, Response} from 'express'
 import cors from 'cors';
 import {SETTINGS} from "./settings";
 import {blogsRouter} from "./blogs/blogs.router";
@@ -8,6 +8,11 @@ import {testingRouter} from "./testing/testing.router";
 export const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
+    res.status(200).json('Welcome to blogger-platform')
+})
 
 app.use(SETTINGS.PATH.BLOGS, blogsRouter);
 app.use(SETTINGS.PATH.POSTS, postsRouter);
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,46 +1,8 @@
 import {dbType} from "../common/types/db.types";
 
 export const db: dbType = {
-    blogs: [
-        {
-            id: '1',
-            name: 'Blog 1',
-            description: 'First blog',
-            websiteUrl: 'https://samurai-blog-1.com',
-        },
-        {
-            id: '2',
-            name: 'Blog 2',
-            description: 'Second blog',
-            websiteUrl: 'https://samurai-blog-2.com'
-        }
-    ],
-    posts: [
-        {
-            id: '1',
-            title: 'Post 1',
-            shortDescription: 'description for post 1',
-            content: 'content for post 1',
-            blogId: '1',
-            blogName: 'Blog 1',
-        },
-        {
-            id: '2',
-            title: 'Post 2',
-            shortDescription: 'description for post 2',
-            content: 'content for post 2',
-            blogId: '1',
-            blogName: 'Blog 1',
-        },
-        {
-            id: '3',
-            title: 'Post 3',
-            shortDescription: 'description for post 3',
-            content: 'content for post 3',
-            blogId: '2',
-            blogName: 'Blog 2',
-        },
-    ]
+    blogs: [],
+    posts: []
 };
 
 // функции для быстрой очистки/заполнения базы данных для тестов
