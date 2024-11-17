3e20d63e65b5e5249b82c32c6aa2841e76025ea3
ibdkn <ibudiakina@gmail.com>
2024-11-17 18:13:05 +0300

    fixed errors with express-validator

--- src/common/middlewares/errors-result.middleware.ts
+++ src/common/middlewares/errors-result.middleware.ts
@@ -1,16 +1,19 @@
-import { validationResult } from 'express-validator';
+import {validationResult} from "express-validator";
 import { Request, Response, NextFunction } from 'express';
 
 export const handleValidationErrors = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
+    console.log(errors.array());
 
     if (!errors.isEmpty()) {
         const formattedErrors = errors
-            .array({onlyFirstError: true})
-            .map((err) => {
-                return {field: (err as any).param, message: err.msg}
-            });
-        res.status(400).json({ errorsMessages: formattedErrors });
+            .array({ onlyFirstError: true })
+            .map((err) => ({
+                field: (err as any).path,
+                message: err.msg,
+            }));
+
+        return res.status(400).json({ errorsMessages: formattedErrors });
     }
 
     next();
--- src/common/validation/blogs-validators.ts
+++ src/common/validation/blogs-validators.ts
@@ -1,28 +1,27 @@
 import {body} from "express-validator";
 
-export const nameValidate = body('name')
-    .isString().withMessage('Name must be a string')
+export const nameValidator = body('name')
     .trim()
     .notEmpty().withMessage('Name is required')
-    .isLength({min: 1, max: 15}).withMessage('Name must be between 1 and 15 symbols');
+    .isString().withMessage('Name must be a string')
+    .isLength({ max: 15 }).withMessage('Name must be at most 15 symbols');
 
-export const descriptionValidate = body('description')
-    .isString().withMessage('Description must be a string')
+export const websiteUrlValidator = body('websiteUrl')
+    .trim()
+    .notEmpty().withMessage('Website URL is required')
+    .isString().withMessage('Website URL must be a string')
+    .isLength({ max: 100 }).withMessage('Website URL must be at most 100 symbols')
+    .matches(/^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[a-zA-Z0-9._-]+)*\/?$/).withMessage('Website URL is invalid');
+
+export const descriptionValidator = body('description')
     .trim()
     .notEmpty().withMessage('Description is required')
-    .isLength({min: 1, max: 100}).withMessage('Description must be between 1 and 100 symbols');
-
-export const websiteUrlValidate = body('websiteUrl')
-    .isString().withMessage('WebsiteUrl must be a string')
-    .trim()
-    .notEmpty().withMessage('WebsiteUrl is required')
-    .isLength({min: 1, max: 100}).withMessage('WebsiteUrl must be between 1 and 1000 symbols')
-    .matches(/^https?:\/\/([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/[a-zA-Z0-9._-]+)*\/?$/)
-    .withMessage('Website URL is invalid');
+    .isString().withMessage('Description must be a string')
+    .isLength({ max: 100 }).withMessage('Description must be at most 100 symbols');
 
 
 export const validateBlogsFields = [
-    nameValidate,
-    descriptionValidate,
-    websiteUrlValidate
+    nameValidator,
+    websiteUrlValidator,
+    descriptionValidator
 ];
