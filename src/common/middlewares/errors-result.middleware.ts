24745f4720658e664ae9df3751a8fac7fcd4b6fb
ibdkn <ibudiakina@gmail.com>
2024-11-17 17:53:10 +0300

    fixed errors

--- src/common/middlewares/errors-result.middleware.ts
+++ src/common/middlewares/errors-result.middleware.ts
@@ -8,7 +8,7 @@ export const handleValidationErrors = (req: Request, res: Response, next: NextFu
         const formattedErrors = errors
             .array({onlyFirstError: true})
             .map((err) => {
-                return {field: (err as any).param, message: err.msg, }
+                return {field: (err as any).param, message: err.msg}
             });
         res.status(400).json({ errorsMessages: formattedErrors });
     }
