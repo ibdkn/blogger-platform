9ee655066be08165bb28b45edea5801aef597401
ibdkn <ibudiakina@gmail.com>
2024-11-29 22:18:38 +0300

    fix bugs

--- src/blogs/blogs.router.ts
+++ src/blogs/blogs.router.ts
@@ -8,10 +8,10 @@ import {validateObjectId} from "../common/middlewares/objectId.validation";
 
 export const blogsRouter = Router();
 
-blogsRouter.get('/?pageNumber=1&pageSize=10&sortBy=createdAt&sortDirection=desc&searchNameTerm=blog', blogsController.getBlogs);
+blogsRouter.get('/', blogsController.getBlogs);
 blogsRouter.get('/:id', validateObjectId, blogsController.getBlog);
-blogsRouter.get('/:blogId/posts?pageNumber=1&pageSize=10&sortBy=createdAt&sortDirection=desc', blogsController.getPostsByBlogId);
+blogsRouter.get('/:blogId/posts', blogsController.getPostsByBlogId);
 blogsRouter.post('/', authMiddleware, ...validateBlogsFields, handleValidationErrors, blogsController.createBlog);
-blogsRouter.post('/:blogId/posts?pageNumber=1&pageSize=10&sortBy=createdAt&sortDirection=desc', authMiddleware, ...validatePostFields, handleValidationErrors, blogsController.createPost);
+blogsRouter.post('/:blogId/posts', authMiddleware, ...validatePostFields, handleValidationErrors, blogsController.createPost);
 blogsRouter.put('/:id', authMiddleware, validateObjectId, ...validateBlogsFields, handleValidationErrors, blogsController.updateBlog);
 blogsRouter.delete('/:id', authMiddleware, validateObjectId, blogsController.deleteBlog);
