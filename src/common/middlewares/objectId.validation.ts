6a67b2626af910a8fbeac23d889708c24791cbaf
ibdkn <ibudiakina@gmail.com>
2024-11-29 22:17:06 +0300

    fix bugs

--- src/common/middlewares/objectId.validation.ts
+++ src/common/middlewares/objectId.validation.ts
@@ -2,11 +2,14 @@ import {NextFunction, Request, Response} from "express";
 import {ObjectId} from "mongodb";
 
 export const validateObjectId = (req: Request, res: Response, next: NextFunction) => {
-    const { id } = req.params;
+    const {id} = req.params;
+
     if (!ObjectId.isValid(id)) {
-        return res.status(400).json({
-            errorsMessages: [{ field: 'id', message: 'Invalid ObjectId' }],
+        res.status(400).json({
+            errorsMessages: [{field: 'id', message: 'Invalid ObjectId'}],
         });
+        return;
     }
+
     next();
-};
+}
