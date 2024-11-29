cce1da8bdb7483581d5be74d060736a3e53f2de8
ibdkn <ibudiakina@gmail.com>
2024-11-29 22:14:21 +0300

    fix bugs

--- src/blogs/blogs.router.ts
+++ src/blogs/blogs.router.ts
@@ -4,12 +4,13 @@ import {authMiddleware} from "../common/middlewares/auth.middleware";
 import {handleValidationErrors} from "../common/middlewares/errors-result.middleware";
 import {validateBlogsFields} from "./blogs.validation";
 import {validatePostFields} from "../posts/posts.validation";
+import {validateObjectId} from "../common/middlewares/objectId.validation";
 
 export const blogsRouter = Router();
 
 blogsRouter.get('/', blogsController.getBlogs);
 blogsRouter.get('/:id', blogsController.getBlog);
-blogsRouter.get('/:blogId/posts?pageNumber=1&pageSize=10&sortBy=createdAt&sortDirection=desc', blogsController.getPostsByBlogId);
+blogsRouter.get('/:blogId/posts', blogsController.getPostsByBlogId);
 blogsRouter.post('/', authMiddleware, ...validateBlogsFields, handleValidationErrors, blogsController.createBlog);
 blogsRouter.post('/:blogId/posts', authMiddleware, ...validatePostFields, handleValidationErrors, blogsController.createPost);
 blogsRouter.put('/:id', authMiddleware, ...validateBlogsFields, handleValidationErrors, blogsController.updateBlog);
