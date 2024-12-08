26a858122fdea1026d901984039587d3a077a67e
ibdkn <ibudiakina@gmail.com>
2024-12-08 19:32:28 +0300

    made edits

--- src/posts/posts.router.ts
+++ src/posts/posts.router.ts
@@ -2,12 +2,15 @@ import {Router} from "express";
 import {postsController} from "./posts.controller";
 import {authMiddleware} from "../common/middlewares/auth.middleware";
 import {handleValidationErrors} from "../common/middlewares/errors-result.middleware";
-import {validatePostFieldsWithBlogId} from "./posts.validation";
+import {validatePostFields} from "./posts.validation";
+import {blogsRouter} from "../blogs/blogs.router";
 
 export const postsRouter = Router();
 
 postsRouter.get('/', postsController.getPosts);
 postsRouter.get('/:id', postsController.getPost);
-postsRouter.post('/', authMiddleware, ...validatePostFieldsWithBlogId, handleValidationErrors, postsController.createPost);
-postsRouter.put('/:id', authMiddleware, ...validatePostFieldsWithBlogId, handleValidationErrors, postsController.updatePost);
+blogsRouter.get('/:blogId/posts', postsController.getPostsByBlogId);
+postsRouter.post('/', authMiddleware, ...validatePostFields, handleValidationErrors, postsController.createPost);
+blogsRouter.post('/:blogId/posts', authMiddleware, ...validatePostFields, handleValidationErrors, postsController.createPostForSpecificBlog);
+postsRouter.put('/:id', authMiddleware, ...validatePostFields, handleValidationErrors, postsController.updatePost);
 postsRouter.delete('/:id', authMiddleware, postsController.deletePost);
